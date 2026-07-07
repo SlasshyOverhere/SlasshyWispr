f8b94fd6879b7bf48b1d05529c9e835c9a9d679d
SlasshyOverhere <5boned00@gmail.com>
2026-07-07 10:24:33 +0530

    Update 1.0.8: UI and dependency changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
-
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 import "./style.css";
 import "./settings.css";
 import { invoke } from "@tauri-apps/api/core";
